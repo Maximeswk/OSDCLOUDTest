f7b57e965db08f6854d998939f0fac7e51a60de3
Maximeswk <96244364+Maximeswk@users.noreply.github.com>
2024-05-22 11:20:11 +0200

    Update AUPOSD.ps1

--- AUPOSD.ps1
+++ AUPOSD.ps1
@@ -1,18 +1,20 @@
-Write-Host  -ForegroundColor Cyan "Starting AUP PC_SALARIE by Exaduo ..."
-Start-Sleep -Seconds 5
+Write-Host -ForegroundColor Green "Starting OSDCloud AUP ..."
+start-sleep -Seconds 3
 
-#Make sure I have the latest OSD Content
-Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
+Write-Host -ForegroundColor Green "Setting Display Resolution to 1600"
+Set-DisRes 1600
+start-sleep -Seconds 2
+
+Write-Host -ForegroundColor Green "Updating OSD Powershell Module"
 Install-Module OSD -Force
 
-Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
+Write-Host -ForegroundColor Green "Importing OSD Powershell Module"
 Import-Module OSD -Force
 
-#Start OSDCloud ZTI the RIGHT way
-Write-Host  -ForegroundColor Cyan "Start OSDCloud with AUP PC_SALARIE by Exaduo Parameters"
-Start-OSDCloud -OSLanguage fr-fr -OSVersion "Windows 10" -OSBuild 22H2 -OSEdition Education -ZTI
+Write-Host -ForegroundColor Green "Start OSD Cloud AUP"
+#Start-OSDCloud -OSLanguage fr-fr -OSName 'Windows 11 22H2 x64' -OSLicense Volume -OSEdition Pro -Zti
+Start-OSDCloud -OSLanguage fr-fr -OSName 'Windows 10 22H2 x64' -OSLicense Retail -OSEdition Education -Zti
 
-#Restart from WinPE
-Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
-Start-Sleep -Seconds 20
+Write-Host -ForegroundColor Green "Restarting ..."
+start-sleep -Seconds 5
 wpeutil reboot
