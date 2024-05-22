a26356b6b42faaa9d39f9b1fb08cf06676ce695c
Maximeswk <96244364+Maximeswk@users.noreply.github.com>
2024-05-22 11:15:50 +0200

    Update AUPOSD.ps1

--- AUPOSD.ps1
+++ AUPOSD.ps1
@@ -1,12 +1,6 @@
 Write-Host  -ForegroundColor Cyan "Starting AUP PC_SALARIE by Exaduo ..."
 Start-Sleep -Seconds 5
 
-#Change Display Resolution for Virtual Machine
-if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
-}
-
 #Make sure I have the latest OSD Content
 Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
 Install-Module OSD -Force
@@ -16,7 +10,7 @@ Import-Module OSD -Force
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Cyan "Start OSDCloud with AUP PC_SALARIE by Exaduo Parameters"
-Start-OSDCloud -OSLanguage fr-fr -OSVersion "Windows 11" -OSBuild 22H2 -OSEdition Education -ZTI
+Start-OSDCloud -OSLanguage fr-fr -OSVersion "Windows 10" -OSBuild 22H2 -OSEdition Education -ZTI
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
