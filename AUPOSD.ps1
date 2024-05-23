d64fea623c1cd8c3c6cf8f23d5cf8332d77e33a6
Maximeswk <96244364+Maximeswk@users.noreply.github.com>
2024-05-23 17:35:38 +0200

    Update AUPOSD.ps1

--- AUPOSD.ps1
+++ AUPOSD.ps1
@@ -12,7 +12,7 @@ Write-Host -ForegroundColor Green "Importing OSD Powershell Module"
 Import-Module OSD -Force
 
 Write-Host -ForegroundColor Green "Start OSD Cloud AUP"
-Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 22H2 x64' -OSLicense Volume -OSEdition Pro -Zti
+Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 22H2 x64' -OSLicense Volume -OSEdition Education -Zti
 #Start-OSDCloud -OSLanguage en-us -OSVersion "Windows 11" -OSBuild 22H2 -OSEdition Education -ZTI
 
 Write-Host -ForegroundColor Green "Restarting ..."
